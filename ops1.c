8eee1673f0879d06fb24995081fb15bcd280c072
mistidevs-alx <mistidevs@gmail.com>
2023-10-20 06:20:23 -0700

    Pop error

--- ops1.c
+++ ops1.c
@@ -124,7 +124,7 @@ stack_t *top;
 
 if (*stack == NULL || stack == NULL)
 {
-	fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
