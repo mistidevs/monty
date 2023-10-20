153a9b16510cc973ad8aabfc701260087846f8d1
mistidevs-alx <mistidevs@gmail.com>
2023-10-20 07:20:26 -0700

    Betty compliant

--- interpreter.c
+++ interpreter.c
@@ -17,45 +17,36 @@ stack_t *stack = NULL;
 while (getline(&line, &len, fp) != -1)
 {
 	if (line[0] == '\0' || line[0] == '\n' || line[0] == '#')
-	{
-		i++, free(line), line = NULL, len = 0;
+	{	i++, free(line), line = NULL, len = 0;
 		continue; }
 	command = unpad(line);
 	if (command[0] == '\0' || command[0] == '\n' || command[0] == '#')
-	{
-		i++, free(command), free(line), line = NULL, len = 0;
+	{	i++, free(command), free(line), line = NULL, len = 0;
 		continue; }
 	ops = strtow(command, " ");
 	if (ops == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
+	{	fprintf(stderr, "Error: malloc failed\n");
 		free(command), free(line), free_stack(stack);
 		return (1); }
 	if (op_check(ops[0]) == 1)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", (i + 1), ops[0]);
+	{	fprintf(stderr, "L%d: unknown instruction %s\n", (i + 1), ops[0]);
 		free_op_list(ops), free(command), free(line), free_stack(stack);
 		return (1); }
 	if (strcmp(ops[0], "push") == 0)
 	{
 		if (ops[1] != NULL && ops[1][0] == '-')
-		{
-			push_neg(&stack, atoi(ops[1])), i++;
-			continue;
-		}
+		{	push_neg(&stack, atoi(ops[1])), i++;
+			continue; }
 		else if (push_check(ops[1]) == 1)
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", i + 1), i++;
 			free_op_list(ops), free(command), free(line), free_stack(stack);
-			return (1); }
-	}
+			return (1); } }
 	if (strcmp(ops[0], "push") == 0)
 		op_select(ops[0])(&stack, atoi(ops[1]));
 	else if (strcmp(ops[0], "push") != 0)
 		op_select(ops[0])(&stack, (i + 1));
-	free(line), line = NULL, len = 0, i++;
-	free_op_list(ops), free(command);
-}
+	free(line), line = NULL, len = 0, i++, free_op_list(ops), free(command); }
 free_stack(stack), free(line);
 return (0);
 }
---
+++ testing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+./monty 1.m
+./monty 2.m
+./monty 3.m
+./monty 4.m
+./monty 5.m
+./monty 6.m
+./monty 7.m
+./monty 8.m
+./monty 9.m
+./monty 10.m
+./monty 11.m
